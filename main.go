73641d9feecec5684c6f0d3697191d4be5c61900
igorbf495 <fel.hacking@gmail.com>
2024-10-04 16:28:57 -0300

    feat: added method params with flag

--- main.go
+++ main.go
@@ -1,32 +1,54 @@
 package main
 
 import (
+    "bytes"
     "flag"
     "fmt"
-    "net/http"
     "io/ioutil"
+    "net/http"
 )
 
-func getRequest(url string) {
-    resp, err := http.Get(url)
+func makeRequest(method, url, body string) {
+    var reqBody *bytes.Buffer
+    if body != "" {
+        reqBody = bytes.NewBuffer([]byte(body))
+    } else {
+        reqBody = nil
+    }
+
+    req, err := http.NewRequest(method, url, reqBody)
+    if err != nil {
+        fmt.Println("Erro ao criar requisição:", err)
+        return
+    }
+
+    // Adicionando cabeçalhos (opcional)
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
     if err != nil {
         fmt.Println("Erro na requisição:", err)
         return
     }
     defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+    responseBody, err := ioutil.ReadAll(resp.Body)
     if err != nil {
         fmt.Println("Erro ao ler o corpo da resposta:", err)
         return
     }
+
     fmt.Println("Status:", resp.Status)
-    fmt.Println("Resposta:", string(body))
+    fmt.Println("Resposta:", string(responseBody))
 }
 
 func main() {
-    url := flag.String("url", "https://jsonplaceholder.typicode.com/posts/1", "URL para requisição GET")
+    method := flag.String("method", "GET", "Método da requisição (GET, POST, PUT, DELETE)")
+    url := flag.String("url", "https://jsonplaceholder.typicode.com/posts/1", "URL para a requisição")
+    body := flag.String("body", "", "Corpo da requisição (para POST e PUT)")
+
     flag.Parse()
-    
-    getRequest(*url)
+
+    makeRequest(*method, *url, *body)
 }
